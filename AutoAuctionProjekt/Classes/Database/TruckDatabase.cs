f1de8eb44ce4913c47730df9553254b81af535be
Morten Aslak <moaslak@gmail.com>
2022-08-30 08:34:54 +0200

    CreateTruck() in progress

--- AutoAuctionProjekt/Classes/Database/TruckDatabase.cs
+++ AutoAuctionProjekt/Classes/Database/TruckDatabase.cs
@@ -8,18 +8,24 @@ namespace AutoAuctionProjekt.Classes
 {
     partial class Database
     {
-        public void CreateTruck()
+        public void CreateTruck(Truck truck)
         {
             SqlConnection connection = new SqlConnection();
             SqlCommand cmd = new SqlCommand("sp_Add_contact", connection);
             cmd.CommandType = CommandType.StoredProcedure;
-
+            cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = truck.Name;
+            cmd.Parameters.Add("@Km", SqlDbType.Decimal).Value = truck.Km;
+            cmd.Parameters.Add("@RegistrationNumber", SqlDbType.VarChar).Value = truck.RegistrationNumber;
+            cmd.Parameters.Add("@Year", SqlDbType.Int).Value = truck.Year;
+            cmd.Parameters.Add("@HasTowbar", SqlDbType.Bit).Value = truck.HasTowbar;
+            //TODO: færdiggør create truck
             //cmd.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = txtFirstName.Text;
             //cmd.Parameters.Add("@LastName", SqlDbType.VarChar).Value = txtLastName.Text;
 
             connection.Open();
             cmd.ExecuteNonQuery();
             connection.Close();
+            new NotImplementedException();
         }
 
         public Truck SelectTruck(UInt32 TruckID)
