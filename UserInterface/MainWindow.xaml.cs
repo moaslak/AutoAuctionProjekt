963e5b40d4a0acd3139d0a1ddb8e96721edaa0bd
stef663k <stef663k@elev.techcollege.dk>
2022-09-21 10:04:59 +0200

    mainwindow

--- UserInterface/MainWindow.xaml.cs
+++ UserInterface/MainWindow.xaml.cs
@@ -51,7 +51,7 @@ namespace UserInterface
 
         private void exitBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
         }
 
         private void creditListBx_SelectionChanged(object sender, SelectionChangedEventArgs e)
