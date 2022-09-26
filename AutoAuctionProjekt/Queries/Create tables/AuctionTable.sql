310a397815af88bcfec77c92df367a124e7515ce
moaslak <moaslak@gmail.com>
2022-09-26 08:16:16 +0200

    create auction table

---
+++ AutoAuctionProjekt/Queries/Create tables/AuctionTable.sql
@@ -0,0 +1,10 @@
+﻿Use AutoAuctionDB
+CREATE TABLE Auctions(
+	ID INT IDENTITY(1,1),
+	VehicleID INT FOREIGN KEY REFERENCES Vehicle(ID),
+	Seller VARCHAR(64) FOREIGN KEY REFERENCES Users(Username),
+	Buyer VARCHAR(64) FOREIGN KEY REFERENCES Users(Username),
+	MinimumPrice DECIMAL(12,2),
+	StandingBid DECIMAL(12,2),
+	PRIMARY KEY(ID)
+)
