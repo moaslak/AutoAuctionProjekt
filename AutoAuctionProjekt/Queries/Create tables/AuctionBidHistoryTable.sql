ef60d8a9483dbc97e51714e729a7e00a6eb98b11
moaslak <moaslak@gmail.com>
2022-09-27 08:13:08 +0200

    table created

---
+++ AutoAuctionProjekt/Queries/Create tables/AuctionBidHistoryTable.sql
@@ -0,0 +1,9 @@
+﻿Use AutoAuctionDB
+CREATE TABLE AuctionBidHistory(
+	ID INT IDENTITY(1,1),
+	AuctionID INT FOREIGN KEY REFERENCES Auctions(ID),
+	CurrentHighestBidder VARCHAR(64) FOREIGN KEY REFERENCES Users(Username),
+	Bid DECIMAL(12,2),
+	BidData Date,
+	PRIMARY KEY(ID)
+)
