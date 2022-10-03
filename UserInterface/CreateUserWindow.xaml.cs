d7ec484c8ec4521fb104b21813fc48b40d5606cb
moaslak <moaslak@gmail.com>
2022-10-03 08:43:56 +0200

    select auctions for USer DONE

--- UserInterface/CreateUserWindow.xaml.cs
+++ UserInterface/CreateUserWindow.xaml.cs
@@ -23,9 +23,10 @@ namespace UserInterface
         public CreateUserWindow()
         {
             InitializeComponent();
+            PrivateUserRdBtn.IsChecked = true;
         }
 
-        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        private void CreateBtn_Click(object sender, RoutedEventArgs e)
         {
             Database database = new Database();
             PrivateUser privateUser = new PrivateUser("", "", "", "");
@@ -35,10 +36,22 @@ namespace UserInterface
             {
                 try
                 {
-                    database.DatabaseCreate(privateUser);
-                    LoginWindow loginWindow = new LoginWindow();
-                    loginWindow.Show();
-                    this.Close();
+                    privateUser.UserName = UserNameTxtbox.Text;
+                    privateUser.Password = CreatePasswordTxtbox.Password;
+                    privateUser.CPRNumber = UserNameTxtbox.Text;
+                    privateUser.Zipcode = ZipcodeTextBox.Text;
+                    privateUser.Balance = 0;
+
+                    if (privateUser.Password == RepeatPasswordTxtBox.Password)
+                    {
+                        database.DatabaseCreate(privateUser);
+                        LoginWindow loginWindow = new LoginWindow();
+                        loginWindow.Show();
+                        this.Close();
+                    }
+                    else
+                        MessageBox.Show("Passwords are not the same!!!");
+                    
                 }
                 catch (Exception ex)
                 {
@@ -50,10 +63,21 @@ namespace UserInterface
             {
                 try
                 {
-                    database.DatabaseCreate(corporateUser);
-                    LoginWindow loginWindow = new LoginWindow();
-                    loginWindow.Show();
-                    this.Close();
+                    corporateUser.UserName = UserNameTxtbox.Text;
+                    corporateUser.Password = CreatePasswordTxtbox.Password;
+                    corporateUser.CVRNumber = UserNameTxtbox.Text;
+                    corporateUser.Zipcode = ZipcodeTextBox.Text;
+                    corporateUser.Balance = 0;
+                    corporateUser.Credit = Convert.ToDecimal(CreditTxtBox.Text);
+                    if (corporateUser.Password == RepeatPasswordTxtBox.Password)
+                    {
+                        database.DatabaseCreate(corporateUser);
+                        LoginWindow loginWindow = new LoginWindow();
+                        loginWindow.Show();
+                        this.Close();
+                    }
+                    else
+                        MessageBox.Show("Passwords are not the same!!!");
                 }
                 catch (Exception ex)
                 {
@@ -79,6 +103,33 @@ namespace UserInterface
         private void RepeatPasswordTxtBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void PrivateUserRdBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            if(PrivateUserRdBtn.IsChecked == true)
+            {
+                CVRlbl.Visibility = Visibility.Hidden;
+                CreditLbl.Visibility = Visibility.Hidden;
+                CreditTxtBox.Visibility = Visibility.Hidden;
+                CorporateUserRdBtn.IsChecked = false;
+
+                CPRlbl.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void CorporateUserRdBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            if (CorporateUserRdBtn.IsChecked == true)
+            {
+                CPRlbl.Visibility = Visibility.Hidden;
+                PrivateUserRdBtn.IsChecked = false;
+
+                CVRlbl.Visibility = Visibility.Visible;
+                CreditLbl.Visibility = Visibility.Visible;
+                CreditTxtBox.Visibility = Visibility.Visible;
+
+            }
         }
     }
 }
